57951b0914fb3909634d0035cd00c71b771991ee
Oleksii Feshchenko <feshchenkooleksiistudy@gmail.com>
2021-08-18 10:54:09 +0200

    LinkedHashMap + TreeMap

--- src/map/Test.java
+++ src/map/Test.java
@@ -1,28 +1,30 @@
 package map;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Test {
     public static void main(String[] args) {
 
         Map<Integer, String> map = new HashMap<>();
 
-        map.put(1, "One");
-        map.put(2, "Two");
-        map.put(3, "Three");
-        map.put(4, "One");
-        map.put(5, "Two");
-        map.put(6, "Three");
-        map.put(7, "One");
-        map.put(8, "Two");
-        map.put(9, "Three");
-        map.put(11, "One");
-        map.put(22, "Two");
-        map.put(33, "Three");
-        map.put(14, "One");
-        map.put(25, "Two");
-        map.put(36, "Three");
+//        map.put(1, "One");
+//        map.put(2, "Two");
+//        map.put(3, "Three");
+//        map.put(4, "One");
+//        map.put(5, "Two");
+//        map.put(6, "Three");
+//        map.put(7, "One");
+//        map.put(8, "Two");
+//        map.put(9, "Three");
+//        map.put(11, "One");
+//        map.put(22, "Two");
+//        map.put(33, "Three");
+//        map.put(14, "One");
+//        map.put(25, "Two");
+//        map.put(36, "Three");
 
 //        System.out.println(map);
 //
@@ -32,9 +34,31 @@ public class Test {
 //
 //        System.out.println(map.get(2));
 
+//        for(Map.Entry<Integer, String> entry : map.entrySet()){
+//            System.out.println(entry.getKey() + " : " + entry.getValue());
+//        }
+
+        Map<Integer, String> linkedHashMap = new LinkedHashMap<>();
+
+        Map <Integer, String> treeMap = new TreeMap<>();
+
+        testMap(treeMap);
+
+    }
+
+    public static void testMap(Map<Integer, String> map){
+        map.put(4, "Bob");
+        map.put(34, "Mike");
+        map.put(23, "Lisa");
+        map.put(557, "Bartek");
+        map.put(364, "Kuba");
+        map.put(24, "Kamil");
+        map.put(8, "Mihal");
+        map.put(17, "Alex");
+
         for(Map.Entry<Integer, String> entry : map.entrySet()){
             System.out.println(entry.getKey() + " : " + entry.getValue());
         }
-
     }
+
 }
